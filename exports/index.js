02c2521a77d4a350ed38df94e4b4a05ee57c0057
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-01-07 15:39:53 +0100

    Remove exports from repo

--- exports/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ws } from './ws.js';
-export { default as http } from './http.js';
-export { default as direct } from './direct.js';
-import 'socket-request-client';
-import '@leofcoin/utils';
--- exports/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as ws } from './ws.js';
-export { default as http } from './http.js';
-export { default as direct } from './direct.js';
